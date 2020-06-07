fe1e6b2d506f0d696b60318ce46cd67c30480f33
Vishal Tulsani <40906718+Vishal-Tulsani@users.noreply.github.com>
2020-06-07 13:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from Nltk_Summarizer import summary
 import json
+import nltk
+
 
 # app
 app = Flask(__name__)
@@ -9,6 +11,7 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST'])
 
 def summarizer():
+    nltl.download()
     file_data = request.args.get('data')
     
     return jsonify(summary(file_data))
