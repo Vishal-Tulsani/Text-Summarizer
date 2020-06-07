dcff97a662ee128fc8ed70fcf3b13c4a6ae97dae
Vishal Tulsani <40906718+Vishal-Tulsani@users.noreply.github.com>
2020-06-07 13:02:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST'])
 
 def summarizer():
-    nltl.download()
+    nltk.download()
     file_data = request.args.get('data')
     
     return jsonify(summary(file_data))
